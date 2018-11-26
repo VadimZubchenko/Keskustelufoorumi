d8d707239dd954186ff5f254d831afcf111260f1
vadimzu <vadim.zubchenko@metropolia.fi>
2018-11-26 22:37:52 +0200

    lab16

---
+++ labs/16 Model-View-Controller/Controller.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vadimzu
+ * Date: 26/11/2018
+ * Time: 17.42
+ */
+
+class Controller
+{
+
+}
---
+++ labs/16 Model-View-Controller/View.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vadimzu
+ * Date: 26/11/2018
+ * Time: 17.43
+ */
